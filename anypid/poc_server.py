456fb1b70f1750d84e9cdacbcf309dbe495356e2
weihong.xu <xuweihong.cn@gmail.com>
2024-08-13 11:23:43 +0800

    use hypercorn to avoid bug of uviconr

--- anypid/poc_server.py
+++ anypid/poc_server.py
@@ -1,5 +1,8 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from anypid.lib.error import HttpError
+import json
+import os
 
 try:
     import toml
@@ -25,18 +28,33 @@ app = FastAPI()
 async def get_provider():
     return {}
 
-@app.get("/doi/{doi}")
+@app.get("/doi/{doi:path}")
 async def get_metadata(doi: str, doi_sdk: Annotated[ChineseDoiSdk, Depends(get_doi_sdk)]):
-    await doi_sdk.get_doi(doi)
+    print(doi)
+    try:
+        await doi_sdk.get_doi(doi)
+    except HttpError as e:
+        raise HTTPException(e.code, e.message)
     return {"doi": doi}
 
-@app.post("/doi")
-async def register_doi(req: dict, doi_sdk: Annotated[ChineseDoiSdk, Depends(get_doi_sdk)]):
+@app.put("/doi/{doi:path}")
+async def register_doi(doi: str, req: dict, doi_sdk: Annotated[ChineseDoiSdk, Depends(get_doi_sdk)]):
+    data_dir = config.get('data_dir', './data')
+    request_file = f'{data_dir}/{doi}.json'
+    os.makedirs(os.path.dirname(request_file), exist_ok=True)
+    with open(request_file, 'w') as fp:
+        json.dump(req, fp, indent=2)
     print(req)
     return {"status": "ok"}
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, port=config.get('port', 8520))
+    import asyncio
+    from hypercorn.config import Config
+    from hypercorn.asyncio import serve
+
+    port=config.get('port', 8520)
+    server_config = Config()
+    server_config.bind = f'0.0.0.0:{port}'
+    asyncio.run(serve(app, config=server_config))
 
